39ec1504a5a64b57fc38a6f373d56dca89dccca4
dev-chpark <xpmxf5@gmail.com>
2022-10-07 20:31:25 +0900

    10/6

--- examples/dom.js
+++ examples/dom.js
@@ -9,12 +9,16 @@ async function main(){
 
 	const $ = cheerio.load(resp.data);	// 2. HTML 을 파싱하고 DOM 생성하기
 	const elements = $('.slide');		// 3. CSS 셀렉터로 원하는 요소 찾기
+	const nextElement = elements.next();
 
 	// 4. 찾은 요소를 순회하면서 요소가 가진 텍스트를 출력하기
 	elements.each((idx, el) => {
 			// 5. text() 메서드를 사용하기 ㅜ이해 Node 객체인 el 을 $ 로 감싸서 cheerio 객체로 변환
 			console.log($(el).text());
 	});
+
+	console.log("==============test===============");
+	console.log($(nextElement).text());
 }
 
 main();
